9c9298f6227cd92f6d18fbc96f581b3c46a287e0
sohamkale99 <56041704+sohamkale99@users.noreply.github.com>
2023-04-05 11:36:14 +0530

    Add files via upload

---
+++ formElments.js
@@ -0,0 +1,50 @@
+export const inputFormElements=[
+    {
+        name:"firstName",placeholder:"Enter first name", label:"First Name", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    },
+    {
+        name:"lastName",placeholder:"Enter last name", label:"Last Name", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    },
+    {
+        name:"email",type:"email",placeholder:"Enter email", label:"Email", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:12
+    },
+    {
+        name:"phone", type:"number",placeholder:"Enter phone number", label:"Phone", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:12
+    },
+    {
+        name:"street",placeholder:"Enter street", label:"Street", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:12
+    },
+    {
+        name:"city",placeholder:"Enter city name", label:"City", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    }, 
+    {
+        name:"state",placeholder:"Enter state", label:"State", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    }, 
+    {
+        name:"country",placeholder:"Enter country name", label:"Country", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    }, 
+    {
+        name:"pincode",placeholder:"Enter pincode number", label:"Pincode", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    },
+    {
+        name:"crop type",placeholder:"Enter crop name", label:"Crop Type", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    },
+    {
+        name:"land area",placeholder:"Land in acres", label:"Land details", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    },
+    {
+        name:"Reason",placeholder:"Reason for claim", label:"Policy Reason", 
+        variant:"outlined", fullWidth:true,required:true, xs:12,sm:6
+    },
+]
